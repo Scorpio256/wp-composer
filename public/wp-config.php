71110fedd1eb936bb406c66ea91beaaa7022d229
Scorpio256 <milan@urbanec.org>
2023-11-11 12:09:53 +0100

    Cleanups

--- public/wp-config.php
+++ public/wp-config.php
@@ -11,36 +11,36 @@ use Symfony\Component\HttpFoundation\Request;
 Dotenv::createImmutable(realpath(__DIR__ . '/../'))->safeLoad();
 
 // Set the environment type.
-define('WP_ENVIRONMENT_TYPE', env('WP_ENVIRONMENT_TYPE', 'production'));
+define('WP_ENVIRONMENT_TYPE', $_ENV['WP_ENVIRONMENT_TYPE']??'production');
 
 // Set the default WordPress theme.
-define('WP_DEFAULT_THEME', env('WP_DEFAULT_THEME', 'wordplate'));
+define('WP_DEFAULT_THEME', $_ENV['WP_DEFAULT_THEME']??'wordplate');
 
 // For developers: WordPress debugging mode.
-$isDebugModeEnabled = env('WP_DEBUG', false);
+$isDebugModeEnabled = $_ENV['WP_DEBUG']?? false;
 define('WP_DEBUG', $isDebugModeEnabled);
-define('WP_DEBUG_LOG', env('WP_DEBUG_LOG', false));
-define('WP_DEBUG_DISPLAY', env('WP_DEBUG_DISPLAY', $isDebugModeEnabled));
-define('SCRIPT_DEBUG', env('SCRIPT_DEBUG', $isDebugModeEnabled));
+define('WP_DEBUG_LOG', $_ENV['WP_DEBUG_LOG']?? false);
+define('WP_DEBUG_DISPLAY', $_ENV['WP_DEBUG_DISPLAY']?? $isDebugModeEnabled);
+define('SCRIPT_DEBUG', $_ENV['SCRIPT_DEBUG']?? $isDebugModeEnabled);
 
 // The database configuration with database name, username, password,
 // hostname charset and database collate type.
-define('DB_NAME', env('DB_NAME'));
-define('DB_USER', env('DB_USER'));
-define('DB_PASSWORD', env('DB_PASSWORD'));
-define('DB_HOST', env('DB_HOST', '127.0.0.1'));
-define('DB_CHARSET', env('DB_CHARSET', 'utf8mb4'));
-define('DB_COLLATE', env('DB_COLLATE', 'utf8mb4_unicode_ci'));
+define('DB_NAME', $_ENV['DB_NAME']);
+define('DB_USER', $_ENV['DB_USER']);
+define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
+define('DB_HOST', $_ENV['DB_HOST']?? '127.0.0.1');
+define('DB_CHARSET', $_ENV['DB_CHARSET']??'utf8mb4');
+define('DB_COLLATE', $_ENV['DB_COLLATE']?? 'utf8mb4_unicode_ci');
 
 // Set the unique authentication keys and salts.
-define('AUTH_KEY', env('AUTH_KEY'));
-define('SECURE_AUTH_KEY', env('SECURE_AUTH_KEY'));
-define('LOGGED_IN_KEY', env('LOGGED_IN_KEY'));
-define('NONCE_KEY', env('NONCE_KEY'));
-define('AUTH_SALT', env('AUTH_SALT'));
-define('SECURE_AUTH_SALT', env('SECURE_AUTH_SALT'));
-define('LOGGED_IN_SALT', env('LOGGED_IN_SALT'));
-define('NONCE_SALT', env('NONCE_SALT'));
+define('AUTH_KEY', $_ENV['AUTH_KEY']);
+define('SECURE_AUTH_KEY', $_ENV['SECURE_AUTH_KEY']);
+define('LOGGED_IN_KEY', $_ENV['LOGGED_IN_KEY']);
+define('NONCE_KEY', $_ENV['NONCE_KEY']);
+define('AUTH_SALT', $_ENV['AUTH_SALT']);
+define('SECURE_AUTH_SALT', $_ENV['SECURE_AUTH_SALT']);
+define('LOGGED_IN_SALT', $_ENV['LOGGED_IN_SALT']);
+define('NONCE_SALT', $_ENV['NONCE_SALT']);
 
 // Detect HTTPS behind a reverse proxy or a load balancer.
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
@@ -48,42 +48,42 @@ if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROT
 }
 
 // Set the home url to the current domain.
-define('WP_HOME', env('WP_HOME', Request::createFromGlobals()->getSchemeAndHttpHost()));
+define('WP_HOME', $_ENV['WP_HOME']?? Request::createFromGlobals()->getSchemeAndHttpHost());
 
 // Set the WordPress directory path.
-define('WP_SITEURL', env('WP_SITEURL', sprintf('%s/%s', WP_HOME, env('WP_DIR', 'wordpress'))));
+define('WP_SITEURL', $_ENV['WP_SITEURL']?? sprintf('%s/%s', WP_HOME, $_ENV['WP_DIR']?? 'wordpress'));
 
 // Set the WordPress content directory path.
-define('WP_CONTENT_DIR', env('WP_CONTENT_DIR', __DIR__));
-define('WP_CONTENT_URL', env('WP_CONTENT_URL', WP_HOME));
+define('WP_CONTENT_DIR', $_ENV['WP_CONTENT_DIR']?? __DIR__);
+define('WP_CONTENT_URL', $_ENV['WP_CONTENT_URL']?? WP_HOME);
 
 // Disable WordPress auto updates.
-define('AUTOMATIC_UPDATER_DISABLED', env('AUTOMATIC_UPDATER_DISABLED', true));
+define('AUTOMATIC_UPDATER_DISABLED', $_ENV['AUTOMATIC_UPDATER_DISABLED']?? true);
 
 // Disable WP-Cron (wp-cron.php) for faster performance.
-define('DISABLE_WP_CRON', env('DISABLE_WP_CRON', false));
+define('DISABLE_WP_CRON', $_ENV['DISABLE_WP_CRON']?? false);
 
 // Prevent file edititing from the dashboard.
-define('DISALLOW_FILE_EDIT', env('DISALLOW_FILE_EDIT', true));
+define('DISALLOW_FILE_EDIT', $_ENV['DISALLOW_FILE_EDIT']?? true);
 
 // Disable plugin and theme updates and installation from the dashboard.
-define('DISALLOW_FILE_MODS', env('DISALLOW_FILE_MODS', true));
+define('DISALLOW_FILE_MODS', $_ENV['DISALLOW_FILE_MODS']?? true);
 
 // Cleanup WordPress image edits.
-define('IMAGE_EDIT_OVERWRITE', env('IMAGE_EDIT_OVERWRITE', true));
+define('IMAGE_EDIT_OVERWRITE', $_ENV['IMAGE_EDIT_OVERWRITE']?? true);
 
 // Disable technical issues emails.
-define('WP_DISABLE_FATAL_ERROR_HANDLER', env('WP_DISABLE_FATAL_ERROR_HANDLER', false));
+define('WP_DISABLE_FATAL_ERROR_HANDLER', $_ENV['WP_DISABLE_FATAL_ERROR_HANDLER']?? false);
 
 // Limit the number of post revisions.
-define('WP_POST_REVISIONS', env('WP_POST_REVISIONS', 2));
+define('WP_POST_REVISIONS', $_ENV['WP_POST_REVISIONS']?? 2);
 
 // Set the absolute path to the WordPress directory.
 if (!defined('ABSPATH')) {
-    define('ABSPATH', sprintf('%s/%s/', __DIR__, env('WP_DIR', 'wordpress')));
+    define('ABSPATH', sprintf('%s/%s/', __DIR__, $_ENV['WP_DIR']?? 'wordpress'));
 }
 
 // Set the database table prefix.
-$table_prefix = env('DB_TABLE_PREFIX', 'wp_');
+$table_prefix = $_ENV['DB_TABLE_PREFIX']?? 'wp_';
 
 require_once ABSPATH . 'wp-settings.php';
